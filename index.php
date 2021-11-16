fc7711c5528a7dcfb94ee810ae9711d9b34b6bdc
rivanimarion <84041926+rivanimarion@users.noreply.github.com>
2021-11-15 23:32:24 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+    require "lib/database.php";
+    require "lib/session.php";
+    require "lib/model.php";
+    require "lib/view.php";
+    require "lib/controller.php";
+    require "lib/app.php";
+
+    require_once "config/config.php";
+
+    
+
+    $app = new App();
